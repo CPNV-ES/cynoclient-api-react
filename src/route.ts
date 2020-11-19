30e9b0668f689a9dfe95978c3dbd453508cbc897
Alexis Haldy <alexis.haldy@gmail.com>
2020-11-19 13:50:30 +0100

    Add controller and generic getter

--- src/route.ts
+++ src/route.ts
@@ -1,7 +1,23 @@
 import * as pingController from "./controller/Ping.controller";
-import {Application} from "express"
+import * as dogController from "./controller/dog.controller";
+import * as clientController from "./controller/client.controller";
+import * as breedController from "./controller/breed.controller";
+import * as diseasesController from "./controller/diseases.controller";
+import {Application} from "express";
 
 export function route(app: Application) {
   app.route("/ping")
     .get(pingController.get);
+
+  app.route("/dogs")
+      .get(dogController.get);
+
+  app.route("/clients")
+      .get(clientController.get);
+
+  app.route("/breed")
+      .get(breedController.get);
+
+  app.route("/diseases")
+      .get(diseasesController.get);
 }
