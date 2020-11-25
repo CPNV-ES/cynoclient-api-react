550933f54145b2f7ba1fe53f62d77af1218ed381
Alexis Haldy <alexis.haldy@gmail.com>
2020-11-25 12:03:44 +0100

    Remove ping

--- src/route.ts
+++ src/route.ts
@@ -1,4 +1,3 @@
-import * as pingController from "./controller/Ping.controller";
 import * as dogController from "./controller/dog.controller";
 import * as clientController from "./controller/client.controller";
 import * as breedController from "./controller/breed.controller";
@@ -6,9 +5,6 @@ import * as diseasesController from "./controller/diseases.controller";
 import { Application } from "express";
 
 export function route(app: Application) {
-    app.route("/ping")
-        .get(pingController.get);
-
     app.route("/dogs")
         .get(dogController.get);
 
