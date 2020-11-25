3732af1481b59b8eab7adc420128aa6c9f4af89b
Alexis Haldy <alexis.haldy@gmail.com>
2020-11-25 11:23:56 +0100

    Change clients to client

--- src/controller/client.controller.ts
+++ src/controller/client.controller.ts
@@ -1,15 +1,15 @@
 import {Request, Response} from "express";
 import {createConnection} from "typeorm";
-import {Clients} from "../entity/Client";
+import {Client} from "../entity/Client";
 
 export async function getAll(req: Request, res: Response) {
     const connection = await createConnection();
-    const clients = await connection.getRepository(Clients).find();
+    const clients = await connection.getRepository(Client).find();
     res.status(200).send({list: clients.map(convertObject)});
 }
 export async function get(req: Request, res: Response) {
     const connection = await createConnection();
-    const client = await connection.getRepository(Clients).findOne(req.params.clientId);
+    const client = await connection.getRepository(Client).findOne(req.params.clientId);
     res.status(200).send(convertObject(client));
 }
 function convertObject(source) {
--- src/entity/Client.ts
+++ src/entity/Client.ts
@@ -1,7 +1,7 @@
 import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
 
-@Entity()
-export class Clients {
+@Entity({name: "clients"})
+export class Client {
     @PrimaryGeneratedColumn({
         type: "int",
     }) 
