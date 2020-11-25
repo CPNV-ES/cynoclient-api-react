d90d33971a01c9ecb2a4279d7882a1070cbb54c4
Alexis Haldy <alexis.haldy@gmail.com>
2020-11-25 10:38:11 +0100

    Remove dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import bodyParser from "body-parser";
 import cors from "cors";
-import dotenv from "dotenv";
 import express from "express";
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {User} from "./entity/User";
 
 import {route} from "./route";
-
-dotenv.config();
 
 const port = 3000;
 const ip = "0.0.0.0";
