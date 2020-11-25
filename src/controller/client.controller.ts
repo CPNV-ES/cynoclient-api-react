6e7571f55fe128f6d01b86b00c94fa8b6477844b
Alexis Haldy <alexis.haldy@gmail.com>
2020-11-25 11:33:49 +0100

    Review + fix client get

--- src/controller/client.controller.ts
+++ src/controller/client.controller.ts
@@ -1,16 +1,24 @@
 import {Request, Response} from "express";
-import {createConnection} from "typeorm";
+import {getConnection} from "./db.controller";
 import {Client} from "../entity/Client";
 
 export async function getAll(req: Request, res: Response) {
-    const connection = await createConnection();
-    const clients = await connection.getRepository(Client).find();
-    res.status(200).send({list: clients.map(convertObject)});
+    const connection = await getConnection();
+    try {
+        const clients = await connection.getRepository(Client).find();
+        res.status(200).send({list: clients.map(convertObject)});
+    } catch(error) {
+        res.status(404).send("Error");
+    }
 }
 export async function get(req: Request, res: Response) {
-    const connection = await createConnection();
-    const client = await connection.getRepository(Client).findOne(req.params.clientId);
-    res.status(200).send(convertObject(client));
+    const connection = await getConnection();
+    try {
+        const client = await connection.getRepository(Client).findOne(req.params.clientId);
+        res.status(200).send(convertObject(client));
+    } catch(error) {
+        res.status(404).send("Client not found");
+    }
 }
 function convertObject(source) {
     return {...source, isFemale: !!source.female}
