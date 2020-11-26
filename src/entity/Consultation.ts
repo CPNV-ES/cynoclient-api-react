0f5c6dabe6ea799b3ac8fc3f8e45a3ec30e3888e
Florian Bergmann <flobergmann@aol.ch>
2020-11-26 13:57:49 +0100

    create consultation entity

---
+++ src/entity/Consultation.ts
@@ -0,0 +1,48 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity({name: 'consultations'})
+export class Breed {
+    @PrimaryGeneratedColumn({
+        type: "int"
+    })  
+    id: number
+    @Column({
+        type: "varchar",
+        length: 2000
+    })  
+    situation: string
+    @Column({
+        type: "varchar",
+        length: 500,
+        nullable: true
+    })  
+    goal: string
+    @Column({
+        type: "varchar",
+        length: 250,
+        nullable: true
+    })  
+    deadline: string
+    @Column({
+        type: "varchar",
+        length: 2000,
+        nullable: true
+    })  
+    solution: string
+    @Column({
+        type: "varchar",
+        length: 100,
+        nullable: true
+    })  
+    medicines: string
+    @Column({
+        type: "varchar",
+        length: 2000,
+        nullable: true
+    })  
+    argumentation: string
+    @Column({
+        type: "int"
+    })  
+    id_service: number
+}
