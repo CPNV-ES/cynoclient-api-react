f3c067a200a0be66193346b3234a606533f50bd5
Florian Bergmann <flobergmann@aol.ch>
2020-12-09 10:20:02 +0100

    add relation breeds<->categories

--- src/entity/Category.ts
+++ src/entity/Category.ts
@@ -1,4 +1,5 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany} from "typeorm";
+import {Breed} from "./Breed"
 
 @Entity({name: 'categories'})
 export class Category {
@@ -11,4 +12,6 @@ export class Category {
         length: 150
     }) 
     noun: string;
+    @OneToMany(() => Breed, breed => breed.category)
+    breeds: Breed[];
 }
